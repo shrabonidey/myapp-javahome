7e0f013a0dfbf70d423bfb7f79b4e264db4ae054
shrabonidey <44965587+shrabonidey@users.noreply.github.com>
2021-06-18 10:48:46 +0530

    Add files via upload

---
+++ R53.tf
@@ -0,0 +1,49 @@
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
+
+  zones = {
+    "terraform-aws-modules-example.com" = {
+      comment = "terraform-aws-modules-examples.com (uat)"
+      tags = {
+        env = "uat"
+      }
+    }
+
+    "myapp.com" = {
+      comment = "myapp.com"
+    }
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = keys(module.zones.route53_zone_zone_id)[0]
+
+  records = [
+    {
+      name    = "apigateway1"
+      type    = "A"
+      alias   = {
+        name    = "d-10qxlbvagl.execute-api.eu-west-1.amazonaws.com"
+        zone_id = "ZLY8HYME6SFAD"
+      }
+    },
+    {
+      name    = ""
+      type    = "A"
+      ttl     = 3600
+      records = [
+        "10.10.10.10",
+      ]
+    },
+  ]
+
+  depends_on = [module.zones]
+}
